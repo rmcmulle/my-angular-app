f50433549c1e4bd5a5cedf63c407d46bb1f762c2
mcmullen-ryan <McMullen_Ryan@bah.com>
2018-02-20 08:34:52 -0500

    Server showing logic

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -20,10 +20,12 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = "Currently no server created";
   serverName = '';
+  serverCreated = false;
 
   // Methods
   onCreateServer() {
-    this.serverCreationStatus = "Server was created! Name is: " + this.serverName
+    this.serverCreationStatus = "Server was created! Name is: " + this.serverName;
+    this.serverCreated = true;
   }
 
   onUpdateServerName(event: any) {
